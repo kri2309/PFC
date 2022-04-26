8f1273bb188c0f6a3473981e7e5bdc7ebf2d912d
kristina <56250919+kri2309@users.noreply.github.com>
2022-04-26 18:59:10 +0200

    Update payments.js

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -12,9 +12,22 @@ const __dirname = dirname(__filename);
 const payments = Express.Router();
 
 payments.route("/").get((req,res) => {
-    const token = req.query;
+   
+    const token = req.query.token;
 
+    validateToken(token)
+    .then((ticket) => {
+        if (ticket.getPayload().name !=null){
+            res.sendFile(path.join(__dirname, "../../frontend/payments.html"));
+
+        } else{
+            res.redirect("/");
+        }
+    }).catch((error) => {
+        console.log("Token expired");
         res.redirect("/");
+   
+    });
    
 });
 
