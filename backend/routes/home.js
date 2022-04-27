dcd122e4f4e7226a7564339d1f04434bdc98ae37
kristina <intern3@stl.com.mt>
2022-04-27 16:57:44 +0200

    Update home.js

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -12,21 +12,8 @@ const __dirname = dirname(__filename);
 const home = Express.Router();
 
 home.route("/").get((req,res) => {
-    const token = req.query.token;
+ res.sendFile(path.join(__dirname, "../../frontend/home.html"));
 
-    validateToken(token)
-    .then((ticket) => {
-        if (ticket.getPayload().name !=null){
-            res.sendFile(path.join(__dirname, "../../frontend/home.html"));
-
-        } else{
-            res.redirect("/");
-        }
-    }).catch((error) => {
-        console.log("Token expired");
-        res.redirect("/");
-   
-    });
 });
 
 export default home;
