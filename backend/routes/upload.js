6fd2c188f262da248f348ca865d92c980bc84c12
kristina <56250919+kri2309@users.noreply.github.com>
2022-04-29 10:49:10 +0200

    Update upload.js

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -146,7 +146,7 @@ upload.route("/").post(imageUpload.single("image"),async function  (req, res)  {
       });
     });
   */
-  var resp = await storage.bucket(bucketname).file(`completed/${NewName}`).save(newfile);
+  const resp = await storage.bucket(bucketname).file(`completed/${NewName}`).save(newfile);
   console.log("hello!: " + resp);
 
     res.send({
