822f32f0b6864f1283fbba741e8d74b78d8c65b6
kristina <intern3@stl.com.mt>
2022-05-01 22:58:26 +0200

    Update fileupload.js

--- frontend/public/assets/js/fileupload.js
+++ frontend/public/assets/js/fileupload.js
@@ -10,5 +10,8 @@ const uploadFile = async () => {
     formData.append("image", fileUpload);
     const response = await axios.post(url, formData, headers);
     console.log(response);
+  }else {
+    alert("Error: File Type not supported!");
+
   }
 };
